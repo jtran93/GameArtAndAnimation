4133dfeaf8a1b07a10e2cba6c353824124f70e9e
jtran93 <johnathan_tran93@yahoo.com>
2016-10-18 00:33:58 -0500

    First import
    Added all work done up to 10/18

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+    public Vector2 speed;
+    Rigidbody2D rb;
+    
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = speed;	
+	}
+	
+	void Update ()
+    {
+        rb.velocity = speed;
+	}
+
+    void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        if(otherObject.gameObject.tag != "Player")
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitController : MonoBehaviour
+{
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
